283846987dcdbfddf4bca0b1b73ead2a023220b2
Fabricio <fabricio872@gmail.com>
2021-08-17 16:49:19 +0200

    added xdebug configuration

--- install.sh
+++ install.sh
@@ -5,16 +5,22 @@ apt-get update
 
 # PHP 8
 apt install -y php-dev php-curl php-xml php-gd php-imagick php-zip php-mysql php-sqlite3 php-pgsql php-intl php-xdebug
+echo -e "\n\nxdebug.mode=debug" | tee -a $(php -i | grep /.+/php.ini -oE)
 # PHP 8
 apt install -y php8.0-dev php8.0-curl php8.0-xml php8.0-gd php8.0-imagick php8.0-zip php8.0-mysql php8.0-sqlite3 php8.0-pgsql php8.0-intl php8.0-xdebug
+echo -e "\n\nxdebug.mode=debug" | tee -a $(php8.0 -i | grep /.+/php.ini -oE)
 # PHP 7.4
 apt install -y php7.4-dev php7.4-curl php7.4-xml php7.4-gd php7.4-imagick php7.4-zip php7.4-mysql php7.4-sqlite3 php7.4-pgsql php7.4-intl php7.4-xdebug
+echo -e "\n\nxdebug.mode=debug" | tee -a $(php7.4 -i | grep /.+/php.ini -oE)
 # PHP 7.3
 apt install -y php7.3-dev php7.3-curl php7.3-xml php7.3-gd php7.3-imagick php7.3-zip php7.3-mysql php7.3-sqlite3 php7.3-pgsql php7.3-intl php7.3-xdebug
+echo -e "\n\nxdebug.mode=debug" | tee -a $(php7.3 -i | grep /.+/php.ini -oE)
 # PHP 7.2
 apt install -y php7.2-dev php7.2-curl php7.2-xml php7.2-gd php7.2-imagick php7.2-zip php7.2-mysql php7.2-sqlite3 php7.2-pgsql php7.2-intl php7.2-xdebug
+echo -e "\n\nxdebug.mode=debug" | tee -a $(php7.2 -i | grep /.+/php.ini -oE)
 # PHP 7.0
 apt install -y php7.0-dev php7.0-curl php7.0-xml php7.0-gd php7.0-imagick php7.0-zip php7.0-mysql php7.0-sqlite3 php7.0-pgsql php7.0-intl php7.0-xdebug
+echo -e "\n\nxdebug.mode=debug" | tee -a $(php7.0 -i | grep /.+/php.ini -oE)
 
 # Composer
 apt install -y unzip
